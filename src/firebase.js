04e4887ddd8e0fc9b431f6005e758cded86514a8
sayam3004 <mutant.sayam>
2026-02-11 18:54:35 +0600

    firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,13 @@ import {
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyDtED7qz9jHmwSkjpuF-vqPzG8KmAChPLY",
+  authDomain: "zipsy-f473d.firebaseapp.com",
+  projectId: "zipsy-f473d",
+  storageBucket: "zipsy-f473d.firebasestorage.app",
+  messagingSenderId: "777775426182",
+  appId: "1:777775426182:web:05b963ed454904c84a3f07",
+  measurementId: "G-S1MPNG1FDT"
 };
 
 const firebaseApp = !getApps().length
